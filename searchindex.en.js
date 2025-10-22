33b4173d170d74376b68b5bafcd3defde2b32034
Leizar06001 <rloussignian@icloud.com>
2025-10-22 17:10:53 +0200

    Deploy 2025-10-22 15:10:53 UTC

---
+++ searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Vin-Tech",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Vin-Tech",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Vin-Tech",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Vin-Tech",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
